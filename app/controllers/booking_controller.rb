bf95fb8562a2e6185b96368b78ee35c4a9814242
Yi-An Ko <yian80413@gmail.com>
2021-11-16 18:13:25 +0100

    create booking done, I am now trying to validate the end date after start date, haven't figured out how to do it yet.

---
+++ app/controllers/booking_controller.rb
@@ -0,0 +1,20 @@
+class BookingController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :pet
-  validates :start_date, presence: true
+  validates :start_date, presence: true, date: { before: :end_date }
   validates :end_date, presence: true
 end
