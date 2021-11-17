c25f2e46d831047b587b78239d849124d4ad0bdd
Akiho Hamamoto <ahamamoto1125@gmail.com>
2021-11-17 12:34:55 +0100

    added the link to main page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @bookings = Booking.where(user_id: @user)
+    @pets = Pet.where(user_id: @user)
   end
 end
