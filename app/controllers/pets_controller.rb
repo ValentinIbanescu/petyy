dfe0047ca311e957d9ecca12bf75713a5fdd5111
Akiho Hamamoto <ahamamoto1125@gmail.com>
2021-11-17 12:51:31 +0100

    added details to the pet page

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,12 +1,13 @@
 class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
+    # @user = Pet.user_id
   end
-  
+
   def index
     @pets = Pet.all
   end
-  
+
   private
 
   def pet_params
