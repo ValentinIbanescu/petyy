d640f7938852e649223b8354f777c6761f2cf80c
Yi-An Ko <yian80413@gmail.com>
2021-11-17 11:00:54 +0100

    I found out the new bookings are not saving, still trying to find the reason

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -1,19 +1,26 @@
 class BookingController < ApplicationController
+  def index
+    Booking.all
+  end
 
   def new
     @booking = Booking.new
   end
 
   def create
+    @pet = Pet.find(params[:pet_id])
     @booking = Booking.new(booking_params)
-    if @booking.save
+    @booking.pet = @booking
+    if @booking.save!
       redirect_to user_path(current_user)
     else
       render :new
     end
   end
 
+
   private
+
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
