763b35ac2da23f09e3e1590e59cc9fe14d1d4cfb
Yi-An Ko <yian80413@gmail.com>
2021-11-17 13:50:14 +0100

    I finished the pet show page, linked it to the booking page, finished the authentication.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,12 +1,13 @@
 class PetsController < ApplicationController
+   skip_before_action :authenticate_user!
   def show
     @pet = Pet.find(params[:id])
   end
-  
+
   def index
     @pets = Pet.all
   end
-  
+
   private
 
   def pet_params
