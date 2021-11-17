1ec3d888cdd759ec4cdf760d26dfdbbfa2994250
Maruan Paschen <maruanpaschen@gmail.com>
2021-11-17 17:01:24 +0100

    please delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @bookings = Booking.where(user_id: @user)
-    @pets = Pet.where(user_id: @user)
+    @bookings = Booking.where(user_id: @user.id)
+    @pets = Pet.where(user_id: @user.id)
   end
 end
