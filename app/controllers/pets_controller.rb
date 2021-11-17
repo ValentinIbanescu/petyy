63be4e266768cb3fd8016ad648fea7c1e715ffc1
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-11-17 11:36:13 +0100

    routes created

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,11 +2,29 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
   end
-  
+
   def index
     @pets = Pet.all
   end
-  
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to pets_path(@pet)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @pet.destroy
+    redirect_to pets_path
+  end
+
   private
 
   def pet_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/pets", to: "pets#index"
   get 'pets/:id', to: 'pets#show', as: 'pet'
+  # As a user, I can add a new pet
+  get '/pets/new', to: 'pets#new', as: :new_pet # display the form
+  post '/pets/', to: 'pets#create'
 
   get "/users/:id", to: "users#show", as: 'user'
 end
