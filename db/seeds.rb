5948955e81ff5ec38522d993b795a2e5d4e3eb65
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-11-18 14:49:19 +0100

    photo error

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+require "open-uri"
+
+
+puts "destroying bookings"
+Booking.destroy_all
+puts "destroying pets"
+Pet.destroy_all
 puts "destroying users"
 User.destroy_all
 
@@ -38,38 +40,47 @@ User.create!(
 
 user_id = rand(User.first.id..User.last.id)
 
-puts "destroying pets"
-Pet.destroy_all
+
 puts "creating pets"
 5.times do
   Pet.create(
     user_id: user_id,
     name: Faker::Name.first_name,
     age: rand(0..100),
-    category: ["cat", "dog", "corckadile", "snake", "tiger", "gorilla"].sample,
-    diet: Faker::Food.dish
+    category: Faker::Creature::Animal.name.capitalize,
+    diet: Faker::Food.dish,
+    medical_situation: Faker::Emotion.noun
   )
 end
+puts "Petes created"
+file = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg')
+
+pets = Pet.all
+pets.each do |pet|
+  sleep 1
+  pet.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+end
+
 puts "#{Pet.count} pets were created successfully."
 
 pet_id = rand(Pet.first.id..Pet.last.id)
 
-puts "destroying pets"
-Booking.destroy_all
-puts "creating bookings"
-5.times do
-  Booking.create(
-    user_id: user_id,
-    pet_id: pet_id,
-    start_date: '20210912102511',
-    end_date: '20210919102511'
-  )
-end
-puts "#{Booking.count} bookings were created successfully."
-booking_id = rand(Booking.first.id..Booking.last.id)
+# puts "destroying pets"
+# Booking.destroy_all
+# puts "creating bookings"
+# 5.times do
+#   Booking.create(
+#     user_id: user_id,
+#     pet_id: pet_id,
+#     start_date: '20210912102511',
+#     end_date: '20210919102511'
+#   )
+# end
+# puts "#{Booking.count} bookings were created successfully."
+# booking_id = rand(Booking.first.id..Booking.last.id)
 
-puts "creating reviews"
-Review.create(
-  booking_id: booking_id,
-  content: "great!"
-)
+# puts "creating reviews"
+# Review.create(
+#   booking_id: booking_id,
+#   content: "great!"
+# )
