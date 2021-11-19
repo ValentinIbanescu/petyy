07b15222ca25356eee6a4bb718e759dc4adeb934
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-11-19 15:26:36 +0100

    seed users only

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,10 @@
 # # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 require "open-uri"
 
-puts "destroying bookings"
-Booking.destroy_all
-puts "destroying pets"
-Pet.destroy_all
+# puts "destroying bookings"
+# Booking.destroy_all
+# puts "destroying pets"
+# Pet.destroy_all
 puts "destroying users"
 User.destroy_all
 
@@ -37,44 +37,44 @@ User.create!(
     last_name: "Miller",
     address:"Berlin 1")
 
-user_id = rand(User.first.id..User.last.id)
+# user_id = rand(User.first.id..User.last.id)
 
-puts "creating pets"
-12.times do
-  Pet.create(
-    user_id: user_id,
-    name: Faker::Name.first_name,
-    age: rand(0..100),
-    category: Faker::Creature::Animal.name.capitalize,
-    diet: Faker::Food.dish,
-    medical_situation: Faker::Emotion.noun
-  )
-end
-puts "Pets created"
-
-
-pets = Pet.all
-pets.each do |pet|
-  file = URI.open('https://picsum.photos/id/237/600/400')
-  pet.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
-  file.close
-end
-
-puts "#{Pet.count} pets were created successfully."
-
-pet_id = rand(Pet.first.id..Pet.last.id)
+# puts "creating pets"
+# 12.times do
+#   Pet.create(
+#     user_id: user_id,
+#     name: Faker::Name.first_name,
+#     age: rand(0..100),
+#     category: Faker::Creature::Animal.name.capitalize,
+#     diet: Faker::Food.dish,
+#     medical_situation: Faker::Emotion.noun
+#   )
+# end
+# puts "Pets created"
 
 
-puts "creating bookings"
-5.times do
-  Booking.create(
-    user_id: user_id,
-    pet_id: pet_id,
-    start_date: '20210912102511',
-    end_date: '20210919102511'
-  )
-end
-puts "#{Booking.count} bookings were created successfully."
+# pets = Pet.all
+# pets.each do |pet|
+#   file = URI.open('https://picsum.photos/id/237/600/400')
+#   pet.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+#   file.close
+# end
+
+# puts "#{Pet.count} pets were created successfully."
+
+# pet_id = rand(Pet.first.id..Pet.last.id)
+
+
+# puts "creating bookings"
+# 5.times do
+#   Booking.create(
+#     user_id: user_id,
+#     pet_id: pet_id,
+#     start_date: '20210912102511',
+#     end_date: '20210919102511'
+#   )
+# end
+# puts "#{Booking.count} bookings were created successfully."
 
 # booking_id = rand(Booking.first.id..Booking.last.id)
 
